4a80820729b1d7d066704b504b2ce0fc317e12c1
zael-mab <zackliwa@gmail.com>
2023-04-08 09:30:25 +0000

    alert component

---
+++ src/component/Alert.tsx
@@ -0,0 +1,37 @@
+import {BiErrorAlt} from 'react-icons/bi';
+import {GrValidate} from 'react-icons/gr';
+import {AiOutlineWarning} from 'react-icons/ai';
+
+const AlertIcons = ({type}: {type: string}) => {
+
+    if (type === 'Success'){
+        return <>
+            <GrValidate />
+        </>
+    }
+    else if (type === 'Warning'){
+        return (<>
+            <AiOutlineWarning />
+        </>);
+    }else{
+        return (<>
+            <BiErrorAlt />
+        </>);
+    }
+}
+
+const Alert = ({type, message}: {type: string, message: string}) => {
+    const alertType = type === 'Error' ? 'alert-error' : (type === 'Warning' ? 'alert-warning' : 'alert-success');
+
+
+  return (
+    <div className={`shadow-lg alert ${alertType}`}>
+        <div>
+            <AlertIcons type={type} />
+            <span>{message}</span>
+        </div>
+    </div>
+  );
+};
+
+export default Alert;
