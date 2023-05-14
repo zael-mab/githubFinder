d38a20f0e904aa42d67c62d8aefc8db3d11af731
zael-mab <zackliwa@gmail.com>
2023-05-14 18:52:10 +0100

    adding domains to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    //add google domain
+    domains: ['api.github.com', 'avatars.githubusercontent.com'],
+  },
 }
 
 module.exports = nextConfig
