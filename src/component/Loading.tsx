1c074bb84353d1964ae249d83a9149555ec8f626
zael-mab <zackliwa@gmail.com>
2023-06-06 17:29:37 +0100

    styling change

--- src/component/Loading.tsx
+++ src/component/Loading.tsx
@@ -1,7 +1,7 @@
 const Loading = () => {
     return (
     <div className='flex items-center justify-center'>
-        <button className="px-6 py-2 text-gray-100 bg-gray-700 border-none rounded-sm btn loading">loading</button>
+        <button className="px-6 py-2 text-gray-100 bg-gray-700 border-none rounded-xl btn loading">loading</button>
     </div>
     );
 };
