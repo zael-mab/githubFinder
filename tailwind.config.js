58bfc25a3cbfb73e19f82a46992cd334718beb73
zael-mab <zackliwa@gmail.com>
2023-07-21 09:20:04 +0100

    no change

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,19 +12,22 @@ module.exports = {
     },
   },
   daisyui: {
-    // themes: [
-    //   {
-    //     light: {
-    //       "primary": "#666666",
-    //       "secondary": "#c0c6f9",
-    //       "accent": "#307ad3",
-    //       "neutral": "#2B2F36",
-    //       "base-100": "#F0F0F0",
-    //       "info": "#6C93EF",
-    //       "success": "#23BE94",
-    //       "warning": "#8F6E0A",
-    //       "error": "#F12229",
-    //     },
+    // themes: [ ,]
+      // {
+        // light: {
+        //   "primary": "#666666",
+        //   "secondary": "#c0c6f9",
+        //   "accent": "#307ad3",
+        //   "neutral": "#2B2F36",
+        //   "base-100": "#F0F0F0",
+        //   "info": "#6C93EF",
+        //   "success": "#23BE94",
+        //   "warning": "#8F6E0A",
+        //   "error": "#F12229",
+        // },
+      // },
+    // ]
+        
   },
   plugins: [require('daisyui')],
 }
